5815bd45a811cafffe1c2bac8063c6a2a2946464
kuerm <kuerm@gmx.net>
2026-03-17 18:26:26 +0000

    Aktualisiere Django-Konfigurationen für Debugging und Host-Zulassungen; füge Umgebungsvariablen für Codespaces hinzu

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,12 @@ SECRET_KEY = 'django-insecure-_dyxs6j60+al+%%xxn$i)vzjp!aat1-^kg1^bh4_$v5270%mx2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+import os
+CODESPACE_NAME = os.environ.get('CODESPACE_NAME')
+codespace_host = f"{CODESPACE_NAME}-8000.app.github.dev" if CODESPACE_NAME else None
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+if codespace_host:
+    ALLOWED_HOSTS.append(codespace_host)
 
 
 # Application definition
